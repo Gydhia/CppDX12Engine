254ee2dcf2a561f6b83fe30388a94b2fdc74c614
Gydhia <60605038+Gydhia@users.noreply.github.com>
2023-12-27 16:16:59 +0100

    Slight adjustements

--- CppDX12Engine/Engine.cpp
+++ CppDX12Engine/Engine.cpp
@@ -1,8 +1,9 @@
-#include <windows.h>
+#include <wrl.h>
+#include <DirectXMath.h>
+#include "Engine.h"
 
 #define MAX_NAME_STRING 256
 #define HInstance() GetModuleHandle(NULL)
-
 
 WCHAR			WindowClass[MAX_NAME_STRING];
 WCHAR			WindowTitle[MAX_NAME_STRING];
@@ -10,89 +11,116 @@ WCHAR			WindowTitle[MAX_NAME_STRING];
 INT				WindowWidth;
 INT				WindowHeight;
 
+using Microsoft::WRL::ComPtr;
 
 
-class Engine {
+int CALLBACK main(HINSTANCE, HINSTANCE, LPSTR, INT)
+{
 
-	int CALLBACK main(HINSTANCE, HINSTANCE, LPSTR, INT)
+	/* - Initialize Global Variables - */
+
+	wcscpy_s(WindowClass, TEXT("TutorialOneClass"));
+	wcscpy_s(WindowTitle, TEXT("Our First Window"));
+	WindowWidth = 1366;
+	WindowHeight = 768;
+
+	/* - Create Window Class - */
+
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
+
+	wcex.hIcon = LoadIcon(0, IDI_APPLICATION);
+	wcex.hIconSm = LoadIcon(0, IDI_APPLICATION);
+
+	wcex.lpszClassName = WindowClass;
+
+	wcex.lpszMenuName = nullptr;
+
+	wcex.hInstance = HInstance();
+
+	wcex.lpfnWndProc = DefWindowProc;
+
+	RegisterClassEx(&wcex);
+
+	/* - Create and Display our Window  - */
+
+	HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, WindowWidth, WindowHeight, nullptr, nullptr, HInstance(), nullptr);
+	if (!hWnd) {
+		MessageBox(0, L"Failed to Create Window!.", 0, 0);
+		return 0; 
+	}
+
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	/* - Listen for Message events - */
+
+	MSG msg = { 0 };
+	while (msg.message != WM_QUIT)
 	{
-
-		/* - Initialize Global Variables - */
-
-		wcscpy_s(WindowClass, TEXT("TutorialOneClass"));
-		wcscpy_s(WindowTitle, TEXT("Our First Window"));
-		WindowWidth = 1366;
-		WindowHeight = 768;
-
-		/* - Create Window Class - */
-
-		WNDCLASSEX wcex;
-
-		wcex.cbSize = sizeof(WNDCLASSEX);
-		wcex.style = CS_HREDRAW | CS_VREDRAW;
-		wcex.cbClsExtra = 0;
-		wcex.cbWndExtra = 0;
-
-		wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-		wcex.hbrBackground = (HBRUSH)GetStockObject(NULL_BRUSH);
-
-		wcex.hIcon = LoadIcon(0, IDI_APPLICATION);
-		wcex.hIconSm = LoadIcon(0, IDI_APPLICATION);
-
-		wcex.lpszClassName = WindowClass;
-
-		wcex.lpszMenuName = nullptr;
-
-		wcex.hInstance = HInstance();
-
-		wcex.lpfnWndProc = DefWindowProc;
-
-		RegisterClassEx(&wcex);
-
-		/* - Create and Display our Window  - */
-
-		HWND hWnd = CreateWindow(WindowClass, WindowTitle, WS_OVERLAPPEDWINDOW,
-			CW_USEDEFAULT, 0, WindowWidth, WindowHeight, nullptr, nullptr, HInstance(), nullptr);
-		if (!hWnd) {
-			MessageBox(0, L"Failed to Create Window!.", 0, 0);
-			return 0; 
-		}
-
-
-		ShowWindow(hWnd, SW_SHOW);
-
-		/* - Listen for Message events - */
-
-		MSG msg = { 0 };
-		while (msg.message != WM_QUIT)
+		// If there are Window messages then process them.
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
 		{
-			// If there are Window messages then process them.
-			if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
-			{
-				TranslateMessage(&msg);
-				DispatchMessage(&msg);
-			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
 		}
-
-
-		return 0;
-
 	}
 
+	return 0;
+}
 
-	void Engine::OnInit()
-	{
-		LoadPipeline();
-		LoadAssets();
-	}
 
-	void Engine::LoadPipeline()
-	{
-		ComPtr
-	}
+void Engine::OnInit()
+{
+	ThrowIfFailed(m_CommandList->Reset(m_DirectCmdListAlloc.Get(), nullptr));
 
-	void Engine::LoadAssets()
-	{
-		
-	}
-};
+	BuildDescriptorHeaps();
+	BuildConstantBuffers();
+	BuildRootSignature();
+	BuildShadersAndInputLayout();
+	BuildBoxGeometry();
+	BuildPSO();
+}
+
+
+
+void Engine::BuildDescriptorHeaps()
+{
+	D3D12_DESCRIPTOR_HEAP_DESC cbvHeapDesc;
+	cbvHeapDesc.NumDescriptors = 1;
+	cbvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+	cbvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+	cbvHeapDesc.NodeMask = 0;
+
+	ThrowIfFailed(m_device->CreateDescriptorHeap(&cbvHeapDesc,
+		IID_PPV_ARGS(&m_DescHeap)));
+}
+
+void Engine::BuildConstantBuffers()
+{
+}
+
+void Engine::BuildRootSignature()
+{
+}
+
+void Engine::BuildShadersAndInputLayout()
+{
+}
+
+void Engine::BuildBoxGeometry()
+{
+}
+
+void Engine::BuildPSO()
+{
+}
+
--- CppDX12Engine/Engine.h
+++ CppDX12Engine/Engine.h
@@ -6,14 +6,16 @@
 #endif
 
 #include <string>
-
-#include "d3dUtil.h"
-#include "GameTimer.h"
+#include <Windows.h>
+#include <d3d12.h>
+#include <windows.h>
+#include <dxgi1_4.h>
 
 // Link necessary d3d12 libraries.
 #pragma comment(lib,"d3dcompiler.lib")
 #pragma comment(lib, "D3D12.lib")
 #pragma comment(lib, "dxgi.lib")
+
 
 class Engine
 {
@@ -25,6 +27,13 @@ public:
     void OnRender();
     void OnDestroy();
 
+    void BuildDescriptorHeaps();
+    void BuildConstantBuffers();
+    void BuildRootSignature();
+    void BuildShadersAndInputLayout();
+    void BuildBoxGeometry();
+    void BuildPSO();
+
     void OnKeyDown(UINT8 /*key*/) {}
     void OnKeyUp(UINT8 /*key*/) {}
 
@@ -35,7 +44,20 @@ public:
 
 
 protected:
-    std::wstring GetAssetFullPath(LPCWSTR assetName);
+    static Engine* m_Engine;
+
+    Microsoft::WRL::ComPtr<IDXGIFactory4> m_dxgiFactory;
+    Microsoft::WRL::ComPtr<ID3D12Device> m_device;
+    Microsoft::WRL::ComPtr<IDXGISwapChain> m_swapChain;
+
+    Microsoft::WRL::ComPtr<ID3D12Fence> m_Fence;
+    UINT64 mCurrentFence = 0;
+
+    Microsoft::WRL::ComPtr<ID3D12CommandQueue> m_CommandQueue;
+    Microsoft::WRL::ComPtr<ID3D12CommandAllocator> m_DirectCmdListAlloc;
+    Microsoft::WRL::ComPtr<ID3D12GraphicsCommandList> m_CommandList;
+
+   
 
 
     // Viewport dimensions.
@@ -47,9 +69,49 @@ protected:
     bool m_useWarpDevice;
 
 private:
+    ComPtr<ID3D12RootSignature> m_RootSignature = nullptr;
+    ComPtr<ID3D12DescriptorHeap> m_DescHeap = nullptr;
+    ComPtr<ID3D12PipelineState> m_PSO = nullptr;
+
     // Root assets path.
     std::wstring m_assetsPath;
 
     // Window title.
     std::wstring m_title;
 };
+
+inline std::wstring AnsiToWString(const std::string& str)
+{
+    WCHAR buffer[512];
+    MultiByteToWideChar(CP_ACP, 0, str.c_str(), -1, buffer, 512);
+    return std::wstring(buffer);
+}
+
+class DxException
+{
+public:
+    DxException() = default;
+    DxException(HRESULT hr, const std::wstring& functionName, const std::wstring& filename, int lineNumber);
+
+    std::wstring ToString()const;
+
+    HRESULT ErrorCode = S_OK;
+    std::wstring FunctionName;
+    std::wstring Filename;
+    int LineNumber = -1;
+};
+
+#define IID_PPV_ARGS(ppType) __uuidof(**(ppType)), IID_PPV_ARGS_Helper(ppType)
+
+#ifndef ThrowIfFailed
+#define ThrowIfFailed(x)                                              \
+{                                                                     \
+    HRESULT hr__ = (x);                                               \
+    std::wstring wfn = AnsiToWString(__FILE__);                       \
+    if(FAILED(hr__)) { throw DxException(hr__, L#x, wfn, __LINE__); } \
+}
+#endif
+
+#ifndef ReleaseCom
+#define ReleaseCom(x) { if(x){ x->Release(); x = 0; } }
+#endif
